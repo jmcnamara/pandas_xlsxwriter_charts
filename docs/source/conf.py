c6467ea3e91935ff078b7ecffb3dd079a681d0e3
John McNamara <jmcnamara@cpan.org>
2014-12-18 12:39:29 +0000

    Added version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2013, John McNamara'
 # built documents.
 #
 # The short X.Y version.
-version = ''
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
